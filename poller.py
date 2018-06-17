1f90f5892997a0bee1eb4ae80765664abf1c208f
annannfryingpan <anngledson@gmail.com>
2018-06-17 11:43:42 +0100

    V minor

--- poller.py
+++ poller.py
@@ -4,9 +4,9 @@ from abc import ABCMeta, abstractmethod
 
 class Poller(object):
     __metaclass__ = ABCMeta
-    def __init__(self, polling_interval):
+    def __init__(self, polling_interval_secs):
         self.running = False
-        self.polling_interval = polling_interval
+        self.polling_interval_secs = polling_interval_secs
 
 
     @abstractmethod
@@ -22,7 +22,7 @@ class Poller(object):
             self.do_work()
 
             work_duration = time.clock() - start
-            time.sleep(max(0, self.polling_interval - work_duration))
+            time.sleep(max(0, self.polling_interval_secs - work_duration))
 
     def stop(self):
         self.running = False
