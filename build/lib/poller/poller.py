9161b5752de00aeabc8541701f388707ba05fed7
annannfryingpan <anngledson@gmail.com>
2018-07-11 17:03:12 +0100

    First go at packaging modules, incl. slight directory restructure

---
+++ build/lib/poller/poller.py
@@ -0,0 +1,28 @@
+import time
+from abc import ABCMeta, abstractmethod
+
+
+class Poller(object):
+    __metaclass__ = ABCMeta
+    def __init__(self, polling_interval_secs):
+        self.running = False
+        self.polling_interval_secs = polling_interval_secs
+
+
+    @abstractmethod
+    def do_work(self):
+        raise NotImplementedError("Must override method: do_work")
+
+
+    def start(self):
+        self.running = True
+        while self.running:
+            start = time.clock()
+
+            self.do_work()
+
+            work_duration = time.clock() - start
+            time.sleep(max(0, self.polling_interval_secs - work_duration))
+
+    def stop(self):
+        self.running = False
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='poller',
+      version='0.1',
+      description='Poller that runs code at a requested frequency',
+      url='https://github.com/AnnAnnFryingPan/poller',
+      author='Ann Gledson',
+      author_email='anngledson@gmail.com',
+      license='MIT',
+      packages=['poller'],
+      zip_safe=False)
