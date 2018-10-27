4274cf9a70e786c0c62e2b4c084e9a7a8a90b033
AnnAnnFryingPan <ann.gledson@manchester.ac.uk>
2018-10-27 14:13:31 +0100

    Updated readme.md

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='poller',
-      version='0.1',
-      description='Poller that runs code at a requested frequency',
+      version='0.2',
+      description='Base class for classes that poll a task regularly, with a constant minimum time interval between each poll. Each polling interval is the maximum of a) polling_interval_secs and b) the time taken to do the task.',
       url='https://github.com/AnnAnnFryingPan/poller',
       author='Ann Gledson',
       author_email='anngledson@gmail.com',
